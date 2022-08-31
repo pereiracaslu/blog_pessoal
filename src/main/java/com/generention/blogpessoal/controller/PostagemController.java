a93782db29582846fb65ddd4c60c0daf3d5685fe
pereiracaslu <lucascosta2512@gmail.com>
2022-08-31 15:22:35 -0300

    Cadastrar e atualizar postagem

--- src/main/java/com/generention/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generention/blogpessoal/controller/PostagemController.java
@@ -6,11 +6,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.generention.blogpessoal.model.Postagem;
 import com.generention.blogpessoal.repository.PostagemRepository;
 import java.util.List;
+
+import javax.validation.Valid;
 
 //Ter acesso a get, putch, delete e post
 @RestController
@@ -36,5 +41,20 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
+	
+	@PostMapping 
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED)
+					.body(postagemRepository.save(postagem));
 	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+}
 
