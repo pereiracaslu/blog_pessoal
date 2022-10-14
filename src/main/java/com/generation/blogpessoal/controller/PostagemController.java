828139027ce28bf59f26d3e41e16c16206f93443
Lucas Pereira <106689837+pereiracaslu@users.noreply.github.com>
2022-10-14 15:53:01 -0300

    Modificando o put

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -68,8 +68,9 @@ public class PostagemController {
 		if (postagemRepository.existsById(postagem.getId())) {
 			
 			if (temaRepository.existsById(postagem.getTema().getId()))
-				
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem));
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
